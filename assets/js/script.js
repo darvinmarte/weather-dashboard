89aa7e486bea3cf51e94ccde7dd3159fe6f39cc2
darvinmarte <119225399+darvinmarte@users.noreply.github.com>
2023-05-12 18:07:34 -0400

    added css

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,11 @@ let currCity = document.querySelector("#currentCity");
 let fiveday = document.querySelector("#fiveDay");
 let inputEl = document.querySelector("#location");
 let button = document.querySelector("#searchButton");
-let fiveDiv = document.querySelector('#fiveDiv')
-let dayOne = document.querySelector('#day1')
-let dayTwo = document.querySelector('#day2')
-let dayThree = document.querySelector('#day3')
-let currentWeatherEl = document.querySelector('#c-weather')
-
+let fiveDiv = document.querySelector("#fiveDiv");
+let dayOne = document.querySelector("#day1");
+let dayTwo = document.querySelector("#day2");
+let dayThree = document.querySelector("#day3");
+let currentWeatherEl = document.querySelector("#c-weather");
 
 function handleUserInput() {
   // get the user input
@@ -24,7 +23,7 @@ function handleUserInput() {
   currentWeather(cityValue);
   forecast(cityValue);
 
-  saveSearch()
+  saveSearch();
 }
 
 function currentWeather(city) {
@@ -40,35 +39,34 @@ function currentWeather(city) {
     })
     .then(function (data) {
       console.log("CURRENT WEATHER ", data);
-      currentWeatherEl.innerHTML = ''
+      currentWeatherEl.innerHTML = "";
       //city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-    
-    var now = dayjs().format("MMMM-DD-YYYY")
-    var nowTime = document.createElement('p')
-    nowTime.textContent = now
-    currentWeatherEl.append(nowTime)
 
-    var icon = data.weather[0].icon
-    var iconUrl = `https://openweathermap.org/img/w/${icon}.png`
-    var iconImage = `<img src='${iconUrl}' alt='${data.weather[0].description}'/>`
-    currentWeatherEl.innerHTML = iconImage
 
+      var icon = data.weather[0].icon;
+      var iconUrl = `https://openweathermap.org/img/w/${icon}.png`;
+      var iconImage = `<img src='${iconUrl}' alt='${data.weather[0].description}'/>`;
+      currentWeatherEl.innerHTML = iconImage;
+
+      var now = dayjs().format("MMMM-DD-YYYY");
+      var nowTime = document.createElement("p");
+      nowTime.textContent = now;
+      currentWeatherEl.append(nowTime);
 
       let cityEl = document.createElement("p");
       cityEl.textContent = data.name;
       currentWeatherEl.append(cityEl);
-      
+
       let todayWeath = document.createElement("div");
-      todayWeath.textContent = 'Temperature: ' + data.main.temp + "℉";
+      todayWeath.textContent = "Temperature: " + data.main.temp + "℉";
       currentWeatherEl.append(todayWeath);
 
       let humidEl = document.createElement("p");
-      humidEl.textContent = 'Humidity: ' + data.main.humidity + '%';
+      humidEl.textContent = "Humidity: " + data.main.humidity + "%";
       currentWeatherEl.append(humidEl);
 
-
-      let windEl = document.createElement('p');
-      windEl.textContent = 'Windspeed: ' + data.wind.speed + " MPH";
+      let windEl = document.createElement("p");
+      windEl.textContent = "Windspeed: " + data.wind.speed + " MPH";
       currentWeatherEl.append(windEl);
     });
 }
@@ -86,18 +84,20 @@ function forecast(city) {
     })
     .then(function (data) {
       console.log("FORECAST ", data);
-      var fiveDayData = data.list.filter(day => day.dt_txt.includes('12:00:00'))
-      console.log(fiveDayData)
-    for( var i = 0; i < fiveDayData.length; i++){
-      var temp1 = document.querySelector('#temp1')
-      temp1.innerHTML = fiveDayData[0].main.temp
+      var fiveDayData = data.list.filter((day) =>
+        day.dt_txt.includes("12:00:00")
+      );
+      console.log(fiveDayData);
+      for (var i = 0; i < fiveDayData.length; i++) {
+        var temp1 = document.querySelector("#temp1");
+        temp1.innerHTML = fiveDayData[0].main.temp;
 
-      var temp2 = document.querySelector('#temp2')
-      temp2.innerHTML = fiveDayData[1].main.temp
+        var temp2 = document.querySelector("#temp2");
+        temp2.innerHTML = fiveDayData[1].main.temp;
 
-      var temp3 = document.querySelector('#temp1')
-      temp1.innerHTML = fiveDayData[0].main.temp
-    }
+        var temp3 = document.querySelector("#temp3");
+        temp3.innerHTML = fiveDayData[2].main.temp;
+      }
     });
 }
 
@@ -105,12 +105,12 @@ function saveSearch(cityName) {
   var storedCity = document.getElementById("location").value;
   localStorage.setItem(cityName, storedCity);
   var createLi = document.createElement("li");
-  createLi.className += "history-btn"
+  createLi.className += "history-btn";
   createLi.textContent = storedCity;
   document.getElementById("history").appendChild(createLi);
 }
 
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
   if (event.target && event.target.matches("#history li")) {
     var listCity = event.target.textContent;
     currentWeather(listCity);
